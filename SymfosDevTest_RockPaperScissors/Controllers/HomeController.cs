0d45b70194f892d078867cd0cd099d158024b0f1
Spirit1230 <tom.osborn1@btinternet.com>
2022-05-07 14:57:34 +0100

    Setup flow for playing game.

--- SymfosDevTest_RockPaperScissors/Controllers/HomeController.cs
+++ SymfosDevTest_RockPaperScissors/Controllers/HomeController.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
-using Common;
 
 namespace SymfosDevTest_RockPaperScissors.Controllers
 {
@@ -25,9 +24,27 @@ namespace SymfosDevTest_RockPaperScissors.Controllers
         }
 
         [HttpPost]
-        public async Task<JsonResult> MakeMove(string lastMove, string difficulty, string playMode)
+        public async Task<JsonResult> MakeMove(string lastMoveP1, string lastMoveP2, string difficulty, string playMode)
         {
-            return Json(new { });
+            Common.Moves p1Move = Common.Moves.Rock;
+            Common.Moves p2Move = Common.Moves.Rock;
+
+            Common.PlayMode playModeEnumType = Common.Enum.GetEnum<Common.PlayMode>(playMode);
+
+            switch (playModeEnumType)
+            {
+                case Common.PlayMode.PlayerVComputer:
+                    p2Move = PlayGame.MakeMove(lastMoveP1, difficulty);
+                    break;
+                case Common.PlayMode.ComputerVComputer:
+                    p1Move = PlayGame.MakeMove(lastMoveP2, difficulty);
+                    p2Move = PlayGame.MakeMove(lastMoveP1, difficulty);
+                    break;
+                default:
+                    break;
+            }
+
+            return Json(new { p1Move = p1Move.ToString(), p2Move = p2Move.ToString() });
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ SymfosDevTest_RockPaperScissors/PlayGame.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common;
+
+namespace SymfosDevTest_RockPaperScissors
+{
+    public class PlayGame
+    {
+        public static Moves MakeMove(string lastMove, string difficulty)
+        {
+            Moves move = Moves.Rock;
+
+            Difficulty diff = Common.Enum.GetEnum<Difficulty>(difficulty);
+
+            switch (diff)
+            {
+                case Difficulty.Begginer:
+                    move = MakeBegginerMove(lastMove);
+                    break;
+                case Difficulty.Intermediate:
+                    move = MakeIntermdeiateMove(lastMove);
+                    break;
+                case Difficulty.Advanced:
+                    move = MakeAdvancedMove(lastMove);
+                    break;
+                default:
+                    break;
+            }
+
+            return move;
+        }
+
+        private static Moves MakeBegginerMove(string lastMove)
+        {
+            return Moves.Rock;
+        }
+
+        private static Moves MakeIntermdeiateMove(string lastMove)
+        {
+            return Moves.Rock;
+        }
+
+        private static Moves MakeAdvancedMove(string lastMove)
+        {
+            return Moves.Rock;
+        }
+    }
+}
